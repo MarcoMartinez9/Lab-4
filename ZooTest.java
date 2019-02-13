1e5207dc5fa0fe3a48d42411ce7b54417a54b76a
MarcoMartinez9 <47228260+MarcoMartinez9@users.noreply.github.com>
2019-02-12 20:32:47 -0600

    Add comments
    Added comments to the ZooTest class. This explains more about the testing that occurred.

--- ZooTest.java
+++ ZooTest.java
@@ -2,10 +2,11 @@ public class ZooTest {
 	
 	public void fullConstructorTest() throws AssertException
 	{
+		//Created a animal object and created a new zoo size. Tested the constructor
 		Animal ani = new Animal("Brown", "Monkey", 10.1, 3.1);
 		Zoo zoo = new Zoo(1);
 		zoo.addAnimal(ani);
-		
+		//Tested getters
 		Assert.assertEquals(10.1, zoo.getAverageWeight(),0.1);
 		Assert.assertEquals(3.1, zoo.getTotalHeight(), 0.1);
 		Assert.assertEquals(1, zoo.getCapacity(), 0.1);
@@ -15,6 +16,7 @@ public class ZooTest {
 	
 	public void testToString() throws AssertException
 	{
+		//Tested toString by adding the animal object to a zoo object. Then, writing  assert that will write the expected information.
 		Animal ani = new Animal("Brown", "Monkey", 10.1, 3.1);
 		Zoo zoo = new Zoo(1);
 		zoo.addAnimal(ani);
