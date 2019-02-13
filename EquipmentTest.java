3ba34080c144e08a6761e44b03733eccf9556ce4
MarcoMartinez9 <47228260+MarcoMartinez9@users.noreply.github.com>
2019-02-12 20:15:55 -0600

    Add comments
    I added comments to my EquipmentTest explaining how I tested equipment class.

--- EquipmentTest.java
+++ EquipmentTest.java
@@ -2,9 +2,9 @@
 public class EquipmentTest {
 	public void ConstructorTest() throws AssertException
     {
-		
+	 //Created Equipment object and tested constructor with Bomb example	
        Equipment bomb = new Equipment("Bomb/1,50.6,70.6,Something you sometimes just can't get rid of");
-
+     // Tested the getters in the constructor
         Assert.assertEquals("Bomb", bomb.getName());
         Assert.assertEquals(1, bomb.getCount(), 0.01);
         Assert.assertEquals(50.6, bomb.getTotalWeight(), 0.01);
@@ -15,6 +15,7 @@ public class EquipmentTest {
 	
 	public void testToString() throws AssertException
 	{
+	 //Tested toString from Equipment
 		Equipment bomb = new Equipment("Bomb/1,50.6,70.60,Something you sometimes just can't get rid of");
 		Assert.assertEquals("Name: Bomb, Number: 1, Weight: 50.60 lbs, Price: $70.60 - Something you sometimes just can't get rid of",bomb.toString());
 	}
